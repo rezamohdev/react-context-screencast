3d058ada98f0dac1b08380decdf609ab87fca61b
Kevin Loughead <kvnloughead@gmail.com>
2021-09-07 13:16:31 -0400

    inital commit

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Profile from './Profile';
+
+const Main = () => {
+  return (
+    <>
+      <Profile />
+    </>
+  );
+};
+
+export default Main;
---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <>
+      <h1>Hello, my name is [username].</h1>
+    </>
+  );
+};
+
+export default Profile;
