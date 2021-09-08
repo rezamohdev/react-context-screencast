a0cf68dce72b47fc3e0d9b7de79e5535025408ef
Kevin Loughead <kvnloughead@gmail.com>
2021-09-07 20:46:49 -0400

    add context provider

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import './App.css';
 import Main from './Main'
+import { TranslationContext, translations } from '../contexts/translationContext';
 
 function App() {
+  const [lang, setLang] = React.useState('en');
+  
   return (
     <div className="App">
-      <Main />
+      <TranslationContext.Provider value={translations[lang]}>
+				<Main />  
+      </TranslationContext.Provider>
     </div>
   );
 }
